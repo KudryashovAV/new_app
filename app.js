2532ba2cc95c1a7de479958362ced010ed998dbf
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2016-02-25 11:21:32 +0300

    Create controller

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var pages = require(__dirname + '/controllers/pages');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +23,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+// app.use('/', routes);
 app.use('/users', users);
+
+// mount routes
+app.get('/', function (req, res) { res.redirect('home') });
+app.get('/home', pages.home);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- tests/test.js
+++ tests/test.js
@@ -15,14 +15,3 @@ describe('foo variable', function () {
     foo.should.equal('bar')
   })
 })
-
-
-var request = require('supertest'),
-    app = require(__dirname + '/../app')
-describe('GET /', function () {
-  it('should contain text "Hello, Express!"', function (done) {
-     request(app)
-       .get('/')
-       .expect(/Hello word!/, done)
-     })
-})
