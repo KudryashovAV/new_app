8149b803b20c653f288dd649504596d5aecca8e0
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2016-02-25 17:18:55 +0300

    Add bootstrap

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var about_us = require(__dirname + '/controllers/about_us');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.set('view options', { layout: true });
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
